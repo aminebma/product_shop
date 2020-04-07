c87d7b3d5b85ffbe7925ae8b6d123322edcee04f
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-04-07 20:00:42 +0100

    Adding a connection test

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const apiController = require('./controllers/apiController')
+const config = require('./config')
 
 const port = process.env.PORT || 3000
 
-
-app.use('/assets', express.static(__dirname + '/public'))
-
 mongoose.connect(config.getDbConnectionString(), {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connection.once('open',function(){
+    console.log('Successfully connected to the database')
+}).on('error', function(error){
+    console.log('Failed to login to the database.\n',error)
+})
+
 apiController(app)
 
 app.listen(port)
---
+++ test/connection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const assert= require('assert')
+const config = require('../config')
+
+before(function(done){
+
+    //Connect to MongoDB
+    mongoose.connect(config.getDbConnectionString(), {useNewUrlParser: true, useUnifiedTopology: true})
+
+    mongoose.connection.once('open', function(){
+        console.log('Connection has been made, now make fireworks !')
+        done()
+    }).on('error', function(error){
+        console.log('Connection error:\n', error)
+    })
+
+})
