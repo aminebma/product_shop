1d61d0a18d808c468488ef5c6e669a3a424b473d
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-04-11 15:14:03 +0100

    Declaring the order lines list outside the loaddata() function

--- app/src/main/java/com/tutorial/product_shop/MainActivity.kt
+++ app/src/main/java/com/tutorial/product_shop/MainActivity.kt
@@ -11,6 +11,8 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
+    private var _orderLines:MutableList<OrderLine> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,23 +21,21 @@ class MainActivity : AppCompatActivity() {
         loadData()
     }
 
-    fun loadData() {
-
-        var data:MutableList<OrderLine> = mutableListOf()
+    private fun loadData() {
 
         val url = "http://10.0.2.2:3000/api/products"
         val request = Request.Builder().url(url).build()
         val client = OkHttpClient()
         client.newCall(request).enqueue(object: Callback{
             override fun onResponse(call: Call, response: Response) {
-                val body = response?.body?.string()
+                val body = response.body?.string()
                 try{
                     val allProducts = JSONArray(body)
                     for(i in 0 until allProducts.length()){
 
                         try{
                             when((allProducts[i] as JSONObject).get("productType")){
-                                "Smartphone" -> data.add(OrderLine(Smartphone((allProducts[i] as JSONObject).getString("brand"), (allProducts[i] as JSONObject).getString("color"),
+                                "Smartphone" -> _orderLines.add(OrderLine(Smartphone((allProducts[i] as JSONObject).getString("brand"), (allProducts[i] as JSONObject).getString("color"),
                                     (allProducts[i] as JSONObject).getString("model"), (allProducts[i] as JSONObject).getString("_id"), (allProducts[i] as JSONObject).getString("name"),
                                     (allProducts[i] as JSONObject).getLong("price"), (allProducts[i] as JSONObject).getInt("qte"))))
                                 "Pack" -> {
@@ -45,7 +45,7 @@ class MainActivity : AppCompatActivity() {
                                         smartphoneList.put(Smartphone((smartphoneListJSON[j] as JSONObject).getString("brand"), (smartphoneListJSON[j] as JSONObject).getString("color"),
                                             (smartphoneListJSON[j] as JSONObject).getString("model"), (smartphoneListJSON[j] as JSONObject).getString("_id"), (smartphoneListJSON[j] as JSONObject).getString("name"),
                                             (smartphoneListJSON[j] as JSONObject).getLong("price"), (smartphoneListJSON[j] as JSONObject).getInt("qte")), (smartphoneListJSON[j] as JSONObject).getInt("quantity"))
-                                    data.add(OrderLine(Pack((allProducts[i] as JSONObject).getString("giftName"), (allProducts[i] as JSONObject).getInt("giftQte"), smartphoneList ,(allProducts[i] as JSONObject).getString("_id"),
+                                    _orderLines.add(OrderLine(Pack((allProducts[i] as JSONObject).getString("giftName"), (allProducts[i] as JSONObject).getInt("giftQte"), smartphoneList ,(allProducts[i] as JSONObject).getString("_id"),
                                         (allProducts[i] as JSONObject).getString("name"), (allProducts[i] as JSONObject).getLong("price"), (allProducts[i] as JSONObject).getInt("qte"))))
                                 }
                             }
@@ -56,7 +56,7 @@ class MainActivity : AppCompatActivity() {
                     }
                     println("Products loaded")
                     runOnUiThread{
-                        recyclerView.adapter = ProductAdapter(this@MainActivity,data)
+                        recyclerView.adapter = ProductAdapter(this@MainActivity,_orderLines)
                     }
                 }catch (e: Exception){
                     println(e.message)
